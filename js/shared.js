38fc283909ede2459313c5912660ee82dcc312a6
Bryan Clark <clarkbw@gmail.com>
2011-08-31 14:40:56 -0700

    force the window to the front if it's already been opened

--- js/shared.js
+++ js/shared.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $("#signin").click(function () {
-    var win = window.open("popup.html", "browserid-signin", "menubar=no,location=yes,resizable=yes,scrollbars=no,status=no,height=375,width=700");
+    window.open("popup.html", "browserid_popup", "menubar=no,location=yes,resizable=yes,scrollbars=no,status=no,height=375,width=700").focus();
   });
 });
