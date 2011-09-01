bd4860f3df24c452be3881faa476837f8ff5ee5d
Andy Chung <animalyouth@gmail.com>
2011-09-01 15:29:28 -0700

    Finished up congrats message

--- js/register.js
+++ js/register.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $(".youraddress").val($.cookie('email'));
   $(window).bind('resize', function() { $('#vAlign').css({'height' : $(window).height() }); }).trigger('resize');
   $("#signUpForm").bind("submit", function () {
-    $(this).children().hide();
+    $(this).hide();
     $("#congrats").fadeIn();
     return false;
   });
