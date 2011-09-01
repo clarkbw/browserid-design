5c0a4a0000ae5d323981f8548d7ced0c8fa27fa0
Bryan Clark <clarkbw@gmail.com>
2011-09-01 15:16:28 -0700

    getting the sign in part done with our arrow animation

--- js/signin.js
+++ js/signin.js
@@ -1,11 +1,39 @@
 $(document).ready(function () {
-  var email = $.cookie('email') || "mozilla@example.com";
+  addEmail($.cookie('email'));
+
+  $("#signInButton").click(function () {
+    $("#signIn").animate({"width" : "685px"}, "slow", function () {
+      // post animation
+      $("body").delay(500).animate({ "opacity" : "0.5"}, "fast", function () {
+        window.close();
+      });
+    });
+    return false;
+  });
+
+  $("#useDifferentEmail").click(function () {
+    $(this).hide();
+    $("div.add").removeClass("left half");
+    $("#differentEmail").css("display", "-moz-box");
+  });
+
+  $("#addDifferentEmail").click(function () {
+    addEmail($("#newEmail").val());
+    $("#differentEmail").hide();
+    return false;
+  });
+
+})
+
+function addEmail(aEmail) {
+  var email = aEmail || "mozilla@example.com";
   var id = email.replace(/[\.@]/g,"_");
+  var $inputs = $("#inputs");
+  $inputs.find("li input").removeAttr("checked");
   $("<li/>").append(
     $("<label />").append(
       $("<input name='email' type='radio' checked />").attr({ "id" : id }),
       " " + email
     ).attr({ "for" : id, "class" : "serif" })
-  ).prependTo($("#inputs"));
-})
-
+  ).prependTo($inputs);
+}
