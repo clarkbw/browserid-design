eb94c851c5d42c449fae793cabbc6835c9e1d7f9
Bryan Clark <clarkbw@gmail.com>
2011-09-01 15:26:49 -0700

    hide the form and show the congrats message

--- js/register.js
+++ js/register.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
   $(".youraddress").val($.cookie('email'));
   $(window).bind('resize', function() { $('#vAlign').css({'height' : $(window).height() }); }).trigger('resize');
+  $("#signUpForm").bind("submit", function () {
+    $(this).children().hide();
+    $("#congrats").fadeIn();
+    return false;
+  });
 })
