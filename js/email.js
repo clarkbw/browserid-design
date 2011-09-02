1d54f3eaf2ade3182a28ede0b2174c0c0b9e8f07
Bryan Clark <clarkbw@gmail.com>
2011-09-02 13:28:41 -0700

    have the email page always check for new cookies

--- js/email.js
+++ js/email.js
@@ -1,12 +1,7 @@
 $(document).ready(function () {
-  if ($.cookie('email')) {
-    $(".youraddress").text($.cookie('email'));
-  } else {
-    var watch = window.setInterval(function () {
-      if ($.cookie('email')) {
-        $(".youraddress").text($.cookie('email'));
-        clearInterval(watch);
-      }
-    }, 5 * 1000);
-  }
+  window.setInterval(function () {
+    if ($.cookie('email')) {
+      $(".youraddress").text($.cookie('email'));
+    }
+  }, 5 * 1000);
 });
