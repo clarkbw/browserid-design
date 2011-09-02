820c05a52d7349af81cf220f826e48c79f9d326c
Bryan Clark <clarkbw@gmail.com>
2011-09-02 14:29:19 -0700

    redirect the signed in flow to the signed in quora page

--- js/shared.js
+++ js/shared.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
   $("#signin").click(function () {
-    window.open("popup.html", "browserid_popup", "menubar=no,location=yes,resizable=yes,scrollbars=no,status=no,height=375,width=700").focus();
+    var win = window.open("popup.html", "browserid_popup", "menubar=no,location=yes,resizable=yes,scrollbars=no,status=no,height=375,width=700");
+    win.focus();
+    var watch = setInterval(function() {
+        if (win.closed) {
+         clearTimeout(watch);
+         $(document).trigger("loggedin");
+        }
+     }, 500);
   });
 });
